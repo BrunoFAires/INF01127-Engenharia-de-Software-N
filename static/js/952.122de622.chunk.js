8e20880e4c392696e81298f9ae2bd8bbccecf2c7
Bruno Aires <brunoferreiraaires@gmail.com>
2024-06-19 17:49:25 -0300

    Updates

---
+++ static/js/952.122de622.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmag=self.webpackChunkmag||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.122de622.chunk.js.map
