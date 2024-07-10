bac2562d9a4f6176cddc6fe79d7566574cea47e7
Bruno <brunoferreiraaires@gmail.com>
2024-07-10 20:39:25 -0300

    Updates

--- static/js/952.122de622.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmag=self.webpackChunkmag||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
-//# sourceMappingURL=952.122de622.chunk.js.map
